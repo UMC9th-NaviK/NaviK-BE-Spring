c200bb2287fa56a9e45dd1fd1aeb58cc16338e06
LeeJaeJun1 <jj030207@naver.com>
2026-01-08 20:05:33 +0900

    feat: Evaluation 도메인 엔티티 추가

---
+++ src/main/java/navik/domain/evaluation/entity/EvaluationTagSelect.java
@@ -0,0 +1,27 @@
+package navik.domain.evaluation.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import navik.domain.study.entity.StudyMember;
+import navik.global.common.BaseEntity;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@Table(name = "evaluationtagselect")
+public class EvaluationTagSelect extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "evaluation_id")
+    private Evaluation evaluation;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "tag_id")
+    private EvaluationTag tag;
+}
---
+++ src/main/java/navik/domain/evaluation/enums/TagType.java
@@ -0,0 +1,14 @@
+package navik.domain.evaluation.enums;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum TagType {
+
+    HIGH_TAGGING("강점태깅"),
+    LOW_TAGGING("약점태깅");
+
+    private final String label;
+}
