345070792528c4a644bd6f1fc772561eb84c538b
LeeJaeJun1 <jj030207@naver.com>
2026-01-08 19:52:38 +0900

    feat: Board 도메인 엔티티 추가

---
+++ src/main/java/navik/domain/board/entity/BoardLike.java
@@ -0,0 +1,31 @@
+package navik.domain.board.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import navik.global.common.BaseEntity;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@Table("board_like")
+public class BoardLike extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id")
+    private Board board;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+}
---
+++ src/main/java/navik/domain/board/entity/Comment.java
@@ -0,0 +1,45 @@
+package navik.domain.board.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import navik.global.common.BaseEntity;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Builder
+@Table(name = "comment")
+
+public class Comment extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "comment_id")
+    private Comment comment;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "board_id")
+    private Board board;
+
+    @Column(name = "comment_content", nullable = false)
+    private String commentContent;
+
+    @Column(name = "comment_deleted", nullable = false)
+    private boolean commentDeleted;
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private LocalDateTime updatedAt;
+}
