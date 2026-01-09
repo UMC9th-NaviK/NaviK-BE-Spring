16e5a8a7a79b5be4b08927b7cf5e5962004880dc
pywoo <kfdsy0103@naver.com>
2026-01-09 21:57:25 +0900

    feat: 네이버 ocr 상수 추가

--- src/main/java/navik/global/ocr/constant/NaverOcrConstant.java
+++ src/main/java/navik/global/ocr/constant/NaverOcrConstant.java
@@ -2,4 +2,7 @@ package navik.global.ocr.constant;
 
 public class NaverOcrConstant {
 	public static final int MAX_PIXEL = 1960;
+	public static final int MAX_MB = 50;
+	public static final String VERSION = "V2";
+	public static final String LANG = "ko";
 }
