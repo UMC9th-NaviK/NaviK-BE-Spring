6769c8918642c8d33ac6a5c7ee8a50e13d3e239f
pywoo <kfdsy0103@naver.com>
2026-01-10 20:35:12 +0900

    chore: 코드 문단 구분

--- src/main/java/navik/global/ocr/service/ImageHelper.java
+++ src/main/java/navik/global/ocr/service/ImageHelper.java
@@ -20,16 +20,16 @@ public class ImageHelper {
 	public ImageMetadata getMetadata(String imageUrl) {
 		try {
 			URL url = new URL(imageUrl);
+
 			URLConnection connection = url.openConnection();
 			connection.setConnectTimeout(TIMEOUT_MS);
 			connection.setReadTimeout(TIMEOUT_MS);
 
-			long fileSize = connection.getContentLengthLong();
 			InputStream stream = connection.getInputStream();
 			ImageInfo imageInfo = Imaging.getImageInfo(stream, "");
 
 			return ImageMetadata.builder()
-				.fileSize(fileSize)
+				.fileSize(connection.getContentLengthLong())
 				.width(imageInfo.getWidth())
 				.height(imageInfo.getHeight())
 				.format(imageInfo.getFormatName().toLowerCase())
