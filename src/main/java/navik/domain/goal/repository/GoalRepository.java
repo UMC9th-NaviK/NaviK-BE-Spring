e112c475a4d37c8ddcf79a9d84c0924017e1d6b6
Junho Kim <2171168@hansung.ac.kr>
2026-01-12 15:27:57 +0900

    [Feat] 목표 설정 api (#26)
    * refactor: 기존 코드 컨벤션 통일
    
    * feat: 액세스 토큰 claim에 status추가(JwtUserDeatils/CustomUserDetailsService/JwtTokenProvider) +코드 포매팅
    
    * feat: 프로필&초기 직무 설정 api
    
    * feat: @AuthUser UserId 반환
    
    * docs: api 스웨거 문서화
    
    * feat: userStatus변경 로직 추가
    
    * feat: 닉네임 중복 체크 api
    
    * feat: 경력 구분 추가
    
    * refactor: 목표 상태 boolean -> enum 변경
    
    * feat: 목표 생성/삭제/조회 API 구현
    
    * fix: isEntryLevel 매핑 문제 해결
    
    * docs: 상태 코드 반영
    
    * fix: 내가 작성한 목표만 수정하도록 변경

---
+++ src/main/java/navik/domain/goal/repository/GoalRepository.java
@@ -0,0 +1,22 @@
+package navik.domain.goal.repository;
+
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import navik.domain.goal.entity.Goal;
+
+import java.util.Optional;
+
+public interface GoalRepository extends JpaRepository<Goal, Long> {
+	Slice<Goal> findByUserIdOrderByCreatedAtDescIdDesc(Long userId, PageRequest pageRequest);
+
+	Slice<Goal> findByUserIdAndIdLessThanOrderByCreatedAtDescIdDesc(Long userId, Long cursor, PageRequest pageRequest);
+
+	Slice<Goal> findByUserIdOrderByEndDateAscIdAsc(Long userId, PageRequest pageRequest);
+
+	Slice<Goal> findByUserIdAndIdLessThanOrderByEndDateAscIdAsc(Long userId, Long cursor, PageRequest pageRequest);
+
+    Optional<Object> findByIdAndUserId(Long userId, Long goalId);
+}
