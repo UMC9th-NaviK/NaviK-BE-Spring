ef2c63b99108510b477b85f929962d9a029f205d
pywoo <kfdsy0103@naver.com>
2026-01-12 18:23:40 +0900

    feat: 임베딩 처리 클래스 작성

---
+++ src/main/java/navik/global/ai/dto/LLMResponseDTO.java
@@ -0,0 +1,50 @@
+package navik.global.ai.dto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import navik.domain.job.enums.JobType;
+import navik.domain.recruitment.enums.AreaType;
+import navik.domain.recruitment.enums.CompanySize;
+import navik.domain.recruitment.enums.EducationType;
+import navik.domain.recruitment.enums.EmploymentType;
+import navik.domain.recruitment.enums.ExperienceType;
+import navik.domain.recruitment.enums.IndustryType;
+
+/**
+ * Structured Output 관리
+ */
+public class LLMResponseDTO {
+
+	@ToString
+	@Getter
+	@NoArgsConstructor
+	public static class Recruitment {
+		private String link;
+		private String title;
+		private String postId;
+		private String companyName;
+		private String companyLogo;
+		private CompanySize companySize;
+		private IndustryType industryType;
+		private LocalDateTime startDate;
+		private LocalDateTime endDate;
+		private List<Position> positions;
+
+		@ToString
+		@Getter
+		@NoArgsConstructor
+		public static class Position {
+			private String name;
+			private JobType jobType;
+			private EmploymentType employmentType;
+			private ExperienceType experienceType;
+			private EducationType educationType;
+			private AreaType areaType;
+			private List<String> kpis;
+		}
+	}
+}
