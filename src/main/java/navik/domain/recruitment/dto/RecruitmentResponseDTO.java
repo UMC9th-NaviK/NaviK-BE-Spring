1961ac0552c08d5609b78de1a07b13eb94e85a0f
pywoo <kfdsy0103@naver.com>
2026-01-15 11:29:51 +0900

    feat: 추천 공고 응답 DTO 정의

---
+++ src/main/java/navik/domain/recruitment/dto/RecruitmentResponseDTO.java
@@ -0,0 +1,21 @@
+package navik.domain.recruitment.dto;
+
+import lombok.Builder;
+import lombok.Getter;
+
+public class RecruitmentResponseDTO {
+
+	@Getter
+	@Builder
+	public static class RecommendPost {
+		private String companyLogo;
+		private String companyName;
+		private String companySize;
+		private Integer deadline;
+		private boolean isSaved;
+		private String title;
+		private String workPlace;
+		private String experience;
+		private String employment;
+	}
+}
