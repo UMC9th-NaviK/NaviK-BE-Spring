55273914b63183123dd3c51e2cecd20d3e6354fa
LeeJaeJun1 <jj030207@naver.com>
2026-01-15 17:05:11 +0900

    chore: api 스웨거 상태코드 추가

--- src/main/java/navik/domain/board/controller/CommentControllerDocs.java
+++ src/main/java/navik/domain/board/controller/CommentControllerDocs.java
@@ -30,6 +30,9 @@ public interface CommentControllerDocs {
     );
 
     @Operation(summary = "댓글 작성", description = "게시글에 댓글 또는 대댓글을 작성합니다.")
+    @io.swagger.v3.oas.annotations.responses.ApiResponses(
+            value = {@io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "201", description = "CREATED")
+            })
     @Parameters({
             @Parameter(name = "boardId", description = "댓글을 작성할 게시글의 ID입니다."),
             @Parameter(name = "userId", description = "게시글을 작성하는 유저 ID입니다", hidden = true)
@@ -41,6 +44,9 @@ public interface CommentControllerDocs {
     );
 
     @Operation(summary = "대댓글 작성", description = "특정 댓글에 대한 답글(대댓글)을 작성합니다.")
+    @io.swagger.v3.oas.annotations.responses.ApiResponses(
+            value = {@io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "201", description = "CREATED")
+            })
     @Parameters({
             @Parameter(name = "boardId", description = "게시글의 ID입니다.", example = "1"),
             @Parameter(name = "commentId", description = "부모 댓글의 ID입니다.", example = "10"),
@@ -54,6 +60,9 @@ public interface CommentControllerDocs {
     );
 
     @Operation(summary = "댓글 삭제", description = "본인이 작성한 댓글을 삭제합니다.")
+    @io.swagger.v3.oas.annotations.responses.ApiResponses(
+            value = {@io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "204", description = "DELETED")
+            })
     @Parameters({
             @Parameter(name = "boardId", description = "게시글의 ID입니다."),
             @Parameter(name = "commentId", description = "삭제할 댓글의 ID입니다."),
