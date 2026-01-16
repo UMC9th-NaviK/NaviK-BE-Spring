057da831c53aeba991daab73f51ef709dfb92af6
pywoo <kfdsy0103@naver.com>
2026-01-16 11:59:09 +0900

    feat: 스웨거 명세 작성

--- src/main/java/navik/domain/recruitment/controller/RecruitmentControllerDocs.java
+++ src/main/java/navik/domain/recruitment/controller/RecruitmentControllerDocs.java
@@ -11,6 +11,9 @@ import navik.global.auth.annotation.AuthUser;
 @Tag(name = "Recruitment", description = "채용 공고 관련 API")
 public interface RecruitmentControllerDocs {
 
+	@io.swagger.v3.oas.annotations.responses.ApiResponses(
+		value = {@io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "200", description = "OK")
+		})
 	@Operation(summary = "사용자 추천 채용 공고 조회", description = "사용자에게 적합한 채용 공고 최대 5건을 조회합니다.")
-	ApiResponse<List<RecruitmentResponseDTO.RecommendPost>> getRecommendedPost(@AuthUser Long userId);
+	ApiResponse<List<RecruitmentResponseDTO.RecommendPost>> getRecommendedPosts(@AuthUser Long userId);
 }
