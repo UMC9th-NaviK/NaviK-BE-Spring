118d48bda44e882aa90e6876b50200bf440b5f7e
bmh7190 <bmh7190@naver.com>
2026-01-18 01:16:26 +0900

    refactor: 성장 로그 조회에서 전체 KPI Score 변화량으로 수정

--- src/main/java/navik/domain/growthLog/repository/GrowthLogRepository.java
+++ src/main/java/navik/domain/growthLog/repository/GrowthLogRepository.java
@@ -7,7 +7,6 @@ import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -17,22 +16,6 @@ import navik.domain.growthLog.enums.GrowthType;
 
 @Repository
 public interface GrowthLogRepository extends JpaRepository<GrowthLog, Long> {
-
-	@Modifying
-	@Query("""
-			update GrowthLog g
-			   set g.kpiCard.id = :kpiCardId,
-			       g.score = :score
-			 where g.id = :growthLogId
-			   and g.type = :type
-			   and g.kpiCard is null
-		""")
-	int applyUserInputResultOnce(
-		@Param("growthLogId") Long growthLogId,
-		@Param("kpiCardId") Long kpiCardId,
-		@Param("score") Integer score,
-		@Param("type") GrowthType type
-	);
 
 	@Query("""
 		select gl
