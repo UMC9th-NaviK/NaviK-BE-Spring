8d1138e9a5df53eb0d7d0e053166713436331ee1
LeeJaeJun1 <jj030207@naver.com>
2026-01-21 01:54:46 +0900

    fix: Docs에 responseCode 추가

--- src/main/java/navik/domain/study/controller/StudyControllerDocs.java
+++ src/main/java/navik/domain/study/controller/StudyControllerDocs.java
@@ -8,5 +8,8 @@ import navik.global.apiPayload.ApiResponse;
 @Tag(name = "Study", description = "스터디 관련 API")
 public interface StudyControllerDocs {
 	@Operation(summary = "스터디 생성 API", description = "새로운 스터디를 생성합니다.")
+	@io.swagger.v3.oas.annotations.responses.ApiResponses(
+		value = {@io.swagger.v3.oas.annotations.responses.ApiResponse(responseCode = "201", description = "CREATED")
+		})
 	ApiResponse<Long> createStudy(StudyCreateDTO.CreateDTO request, Long userId);
 }
