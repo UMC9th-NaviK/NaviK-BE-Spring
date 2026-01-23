d4ca0f1cefdbd641b5432d865590f91275365d23
pywoo <kfdsy0103@naver.com>
2026-01-23 14:00:30 +0900

    feat: 채용 공고 dto 정의

---
+++ src/main/java/navik/domain/recruitment/dto/recruitment/RecruitmentRequestDTO.java
@@ -0,0 +1,56 @@
+package navik.domain.recruitment.dto.recruitment;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import lombok.Builder;
+import lombok.Getter;
+import navik.domain.recruitment.enums.AreaType;
+import navik.domain.recruitment.enums.CompanySize;
+import navik.domain.recruitment.enums.EmploymentType;
+import navik.domain.recruitment.enums.ExperienceType;
+import navik.domain.recruitment.enums.IndustryType;
+import navik.domain.recruitment.enums.JobType;
+import navik.domain.recruitment.enums.MajorType;
+import navik.domain.users.enums.EducationLevel;
+
+public class RecruitmentRequestDTO {
+
+	@Getter
+	@Builder
+	public static class Recruitment {
+
+		private String link;
+		private String title;
+		private String postId;
+		private String companyName;
+		private String companyLogo;
+		private CompanySize companySize;
+		private IndustryType industryType;
+		private LocalDateTime startDate;
+		private LocalDateTime endDate;
+		private List<Position> positions;
+		private String summary;
+
+		@Getter
+		@Builder
+		public static class Position {
+			private String name;
+			private JobType jobType;
+			private EmploymentType employmentType;
+			private ExperienceType experienceType;
+			private EducationLevel educationLevel;
+			private AreaType areaType;
+			private String detailAddress;
+			private MajorType majorType;
+			private List<KPI> kpis;
+		}
+
+		@Getter
+		@Builder
+		public static class KPI {
+			private String kpi;
+			private float[] embedding;
+		}
+	}
+}
