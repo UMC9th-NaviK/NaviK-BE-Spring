711ce74fb9782a717e3f92fddc0a1f1730f6eab9
pywoo <kfdsy0103@naver.com>
2026-01-23 16:58:13 +0900

    feat: positionkpiembedding 컨버터 작성

---
+++ src/main/java/navik/domain/recruitment/converter/position/PositionKpiEmbeddingConverter.java
@@ -0,0 +1,15 @@
+package navik.domain.recruitment.converter.position;
+
+import navik.domain.recruitment.dto.recruitment.RecruitmentRequestDTO;
+import navik.domain.recruitment.entity.PositionKpi;
+import navik.domain.recruitment.entity.PositionKpiEmbedding;
+
+public class PositionKpiEmbeddingConverter {
+
+	public static PositionKpiEmbedding toEntity(PositionKpi positionKpi, RecruitmentRequestDTO.Recruitment.KPI kpi) {
+		return PositionKpiEmbedding.builder()
+			.positionKpi(positionKpi)
+			.embedding(kpi.getEmbedding())
+			.build();
+	}
+}
