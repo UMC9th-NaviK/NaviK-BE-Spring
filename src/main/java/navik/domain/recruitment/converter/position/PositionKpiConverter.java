b7545cf9636f85debe30ac027790acc37823f7d4
pywoo <kfdsy0103@naver.com>
2026-01-23 16:56:57 +0900

    feat: positionkpi 컨버터 작성

---
+++ src/main/java/navik/domain/recruitment/converter/position/PositionKpiConverter.java
@@ -0,0 +1,15 @@
+package navik.domain.recruitment.converter.position;
+
+import navik.domain.recruitment.dto.recruitment.RecruitmentRequestDTO;
+import navik.domain.recruitment.entity.Position;
+import navik.domain.recruitment.entity.PositionKpi;
+
+public class PositionKpiConverter {
+
+	public static PositionKpi toEntity(Position position, RecruitmentRequestDTO.Recruitment.KPI kpi) {
+		return PositionKpi.builder()
+			.position(position)
+			.content(kpi.getKpi())
+			.build();
+	}
+}
