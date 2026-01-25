8840fd25e36902b23f3727eb21268ff5b849d2c4
pywoo <kfdsy0103@naver.com>
2026-01-25 20:26:23 +0900

    feat: majorType에 대한 JsonCreator 작성

--- src/main/java/navik/domain/recruitment/exception/code/RecruitmentErrorCode.java
+++ src/main/java/navik/domain/recruitment/exception/code/RecruitmentErrorCode.java
@@ -15,7 +15,8 @@ public enum RecruitmentErrorCode implements BaseCode {
 	EMPLOYMENT_TYPE_NOT_FOUND(HttpStatus.NOT_FOUND, "RECRUITMENT_404_03", "존재하지 않는 고용 형태입니다."),
 	EXPERIENCE_TYPE_NOT_FOUND(HttpStatus.NOT_FOUND, "RECRUITMENT_404_04", "존재하지 않는 경력 유형입니다."),
 	INDUSTRY_TYPE_NOT_FOUND(HttpStatus.NOT_FOUND, "RECRUITMENT_404_05", "존재하지 않는 업종 유형입니다."),
-	JOB_TYPE_NOT_FOUND(HttpStatus.NOT_FOUND, "RECRUITMENT_404_06", "존재하지 않는 직무 유형입니다.");
+	JOB_TYPE_NOT_FOUND(HttpStatus.NOT_FOUND, "RECRUITMENT_404_06", "존재하지 않는 직무 유형입니다."),
+	MAJOR_TYPE_NOT_FOUND(HttpStatus.NOT_FOUND, "RECRUITMENT_404_07", "존재하지 않는 전공 유형입니다.");
 
 	private final HttpStatus httpStatus;
 	private final String code;
