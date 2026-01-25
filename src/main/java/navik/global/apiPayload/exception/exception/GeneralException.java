37706f45ec3d4dd2ea8edf82be80737e20dcaf02
pywoo <kfdsy0103@naver.com>
2026-01-25 20:47:20 +0900

    fix: GeneralExceptionHandler -> GeneralException rename 및 super(message) 누락

--- src/main/java/navik/global/apiPayload/exception/exception/GeneralException.java
+++ src/main/java/navik/global/apiPayload/exception/exception/GeneralException.java
@@ -1,15 +1,18 @@
 package navik.global.apiPayload.exception.exception;
 
-import navik.global.apiPayload.exception.code.BaseCode;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import navik.global.apiPayload.exception.code.BaseCode;
 
 /**
  * 비즈니스 로직 실행 중 발생하는 예외를 표현하는 클래스입니다.
  * {@link BaseCode}를 포함하여, 예외에 대한 구체적인 정보를 제공합니다.
  */
 @Getter
-@AllArgsConstructor
-public class GeneralExceptionHandler extends RuntimeException {
+public class GeneralException extends RuntimeException {
 	private final BaseCode code;
+
+	public GeneralException(BaseCode baseCode) {
+		super(baseCode.getMessage());
+		this.code = baseCode;
+	}
 }
