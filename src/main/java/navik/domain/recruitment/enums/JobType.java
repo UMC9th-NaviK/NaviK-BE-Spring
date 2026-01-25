94fe31ed9a8c4d717cd2f3ee393c27b9c71ff485
pywoo <kfdsy0103@naver.com>
2026-01-25 20:25:17 +0900

    feat: jobType에 대한 JsonCreator 작성

--- src/main/java/navik/domain/recruitment/enums/JobType.java
+++ src/main/java/navik/domain/recruitment/enums/JobType.java
@@ -1,7 +1,13 @@
 package navik.domain.recruitment.enums;
 
+import java.util.Arrays;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import navik.domain.recruitment.exception.code.RecruitmentErrorCode;
+import navik.global.apiPayload.exception.handler.GeneralExceptionHandler;
 
 @Getter
 @RequiredArgsConstructor
@@ -13,4 +19,12 @@ public enum JobType {
 	BACKEND("백엔드 개발자");
 
 	private final String label;
+
+	@JsonCreator
+	public static JobType deserialize(String jobType) {
+		return Arrays.stream(values())
+			.filter(type -> type.name().equalsIgnoreCase(jobType))
+			.findAny()
+			.orElseThrow(() -> new GeneralExceptionHandler(RecruitmentErrorCode.JOB_TYPE_NOT_FOUND));
+	}
 }
--- src/main/java/navik/domain/recruitment/exception/code/RecruitmentErrorCode.java
+++ src/main/java/navik/domain/recruitment/exception/code/RecruitmentErrorCode.java
@@ -14,7 +14,8 @@ public enum RecruitmentErrorCode implements BaseCode {
 	COMPANY_SIZE_NOT_FOUND(HttpStatus.NOT_FOUND, "RECRUITMENT_404_02", "존재하지 않는 회사 규모입니다."),
 	EMPLOYMENT_TYPE_NOT_FOUND(HttpStatus.NOT_FOUND, "RECRUITMENT_404_03", "존재하지 않는 고용 형태입니다."),
 	EXPERIENCE_TYPE_NOT_FOUND(HttpStatus.NOT_FOUND, "RECRUITMENT_404_04", "존재하지 않는 경력 유형입니다."),
-	INDUSTRY_TYPE_NOT_FOUND(HttpStatus.NOT_FOUND, "RECRUITMENT_404_05", "존재하지 않는 업종 유형입니다.");
+	INDUSTRY_TYPE_NOT_FOUND(HttpStatus.NOT_FOUND, "RECRUITMENT_404_05", "존재하지 않는 업종 유형입니다."),
+	JOB_TYPE_NOT_FOUND(HttpStatus.NOT_FOUND, "RECRUITMENT_404_06", "존재하지 않는 직무 유형입니다.");
 
 	private final HttpStatus httpStatus;
 	private final String code;
