e61aea2035d7ffe930c22160aba90d8fd8566295
pywoo <kfdsy0103@naver.com>
2026-01-25 16:50:34 +0900

    fix: positionkpi repository 잘못된 타입 상속 해결

--- src/main/java/navik/domain/recruitment/repository/position/positionKpi/PositionKpiRepository.java
+++ src/main/java/navik/domain/recruitment/repository/position/positionKpi/PositionKpiRepository.java
@@ -2,7 +2,7 @@ package navik.domain.recruitment.repository.position.positionKpi;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import navik.domain.recruitment.entity.Position;
+import navik.domain.recruitment.entity.PositionKpi;
 
-public interface PositionKpiRepository extends JpaRepository<Position, Long>, PositionKpiCustomRepository {
+public interface PositionKpiRepository extends JpaRepository<PositionKpi, Long>, PositionKpiCustomRepository {
 }
