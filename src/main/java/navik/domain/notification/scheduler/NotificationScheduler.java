c48c7c5723c5deaf5e195827cada5b5d488cd429
pywoo <kfdsy0103@naver.com>
2026-01-26 23:53:29 +0900

    feat: 모든 유저에 대한 알림 처리 스케쥴러 작성

--- src/main/java/navik/domain/notification/scheduler/NotificationScheduler.java
+++ src/main/java/navik/domain/notification/scheduler/NotificationScheduler.java
@@ -1,5 +1,12 @@
 package navik.domain.notification.scheduler;
 
+import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import navik.domain.goal.entity.Goal;
@@ -7,13 +14,8 @@ import navik.domain.goal.repository.GoalRepository;
 import navik.domain.notification.config.NotificationConfig;
 import navik.domain.notification.entity.NotificationType;
 import navik.domain.notification.service.NotificationCommandService;
-
-import org.springframework.scheduling.annotation.Scheduled;
-import org.springframework.stereotype.Component;
-
-import java.time.LocalDate;
-import java.util.List;
-import java.util.stream.Collectors;
+import navik.domain.notification.service.NotificationFacade;
+import navik.domain.users.repository.UserRepository;
 
 @Slf4j
 @Component
@@ -23,6 +25,8 @@ public class NotificationScheduler {
 	private final GoalRepository goalRepository;
 	private final NotificationCommandService notificationCommandService;
 	private final NotificationConfig notificationConfig;
+	private final NotificationFacade notificationFacadeService;
+	private final UserRepository userRepository;
 
 	/**
 	 * 매일 오전 9시에 알림 대상 목표의 마감일을 체크하여 D-day 알림 생성
@@ -60,5 +64,24 @@ public class NotificationScheduler {
 		// 2. 스터디에 참여한 사용자들에 대해 : List<StudyUser>
 		// 3. notificationCommandService.createCompletionNotification(userId, study);
 
+	}
+
+	/**
+	 * 매일 오전 10시 모든 유저에 대해 추천 공고 1건을 조회하고
+	 * D-1 or D-5 남은 경우 개별 알림을 생성합니다.
+	 */
+	@Scheduled(cron = "0 0 10 * * *")
+	public void checkRecommenedRecruitment() {
+
+		// 메모리 고려 id만 가져오고, 유저 개별 트랜잭션을 타도록 facade
+		userRepository.findAllIds().forEach(
+			userId -> {
+				try {
+					notificationFacadeService.checkRecommendedRecruitment(userId);
+				} catch (Exception e) {
+					log.error("추천 공고 알림 생성 실패 - User ID: {}", userId, e);
+				}
+			}
+		);
 	}
 }
