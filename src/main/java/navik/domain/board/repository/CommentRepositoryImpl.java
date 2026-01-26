cb16f725695cc8ab5665de8393d242f34a7193a6
LeeJaeJun1 <jj030207@naver.com>
2026-01-26 18:35:12 +0900

    fix: where절에서 parentCommentId -> boardId 동일한 것으로 변경

--- src/main/java/navik/domain/board/repository/CommentRepositoryImpl.java
+++ src/main/java/navik/domain/board/repository/CommentRepositoryImpl.java
@@ -22,24 +22,24 @@ public class CommentRepositoryImpl implements CommentCustomRepository {
 	QComment comment = QComment.comment;
 
 	@Override
-	public Page<Comment> findByParentCommentId(Long parentCommentId, Pageable pageable) {
+	public Page<Comment> findByBoardId(Long boardId, Pageable pageable) {
 		List<Comment> result = queryFactory.
 			selectFrom(comment)
-			.leftJoin(comment.parentComment) // comment와 parentComment 조인
+			.leftJoin(comment.parentComment).fetchJoin()
+			.leftJoin(comment.user).fetchJoin() // comment와 parentComment 조인
 			.where(
-				comment.parentComment.id.eq(parentCommentId)
+				comment.board.id.eq(boardId) // 게시글 ID로 전체 조회함
 			)
 			.orderBy(
 				// 1. 그룹화(부모 댓글과 그 자식 댓글들이 같은 번호로 묶임)
-				Expressions.stringTemplate(
-					"function('COALESCE', {0}, {1})",
-					comment.parentComment.id, comment.id
-				).asc(),
+				Expressions.numberTemplate(Long.class, "COALESCE({0}, {1})",
+					comment.parentComment.id, comment.id).asc(),
+
 				// 2. 그룹화 후, 부모 우선(CASE WHEN, 부모 댓글이 항상 자식보다 먼저)
-				Expressions.stringTemplate(
-					"case when {0} is null then 0 else 1 end",
-					comment.parentComment.id
-				).asc(),
+				Expressions.numberTemplate(Integer.class,
+					"CASE WHEN {0} is NULL THEN 0 ELSE 1 END",
+					comment.parentComment.id).asc(),
+
 				// 3. 순서보장을 위해 자식 중에서 오래된 순으로 정렬
 				comment.createdAt.asc()
 			)
@@ -52,7 +52,7 @@ public class CommentRepositoryImpl implements CommentCustomRepository {
 			.select(comment.count())
 			.from(comment)
 			.where(
-				comment.parentComment.id.eq(parentCommentId)
+				comment.board.id.eq(boardId)
 			);
 		return PageableExecutionUtils.getPage(result, pageable, countQuery::fetchOne);
 		// PageableExecutionUtils : 페이징 처리를 위한 최적화 유틸리티, 불필요한 전체 개수 조회 쿼리 생략
