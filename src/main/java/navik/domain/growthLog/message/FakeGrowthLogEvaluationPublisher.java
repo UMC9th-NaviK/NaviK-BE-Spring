d924c53aeb05405aeb4b7d52759a6e8d7f26a008
bmh7190 <bmh7190@naver.com>
2026-01-27 16:06:07 +0900

    chore: 비동기 모드용 Fake publisher 구현 추가

---
+++ src/main/java/navik/domain/growthLog/message/FakeGrowthLogEvaluationPublisher.java
@@ -0,0 +1,16 @@
+package navik.domain.growthLog.message;
+
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConditionalOnProperty(name = "navik.growth-log.evaluation-mode", havingValue = "async")
+public class FakeGrowthLogEvaluationPublisher implements GrowthLogEvaluationPublisher {
+
+	@Override
+	public void publish(GrowthLogEvaluationMessage message) {
+		// TODO: Redis Stream/SQS 구현으로 교체
+		// 지금은 비동기 플로우 DI/스위칭만 검증하기 위한 noop
+	}
+
+}
