038bfdf7208104c403bcb73d76833585d18bb386
LeeJaeJun1 <jj030207@naver.com>
2026-01-28 15:53:55 +0900

    feat: 평가하려는 스터디에 참여한 인원을 조회할 수 있는 findUserByStudyId 메서드 추가

--- src/main/java/navik/domain/study/repository/StudyUserRepository.java
+++ src/main/java/navik/domain/study/repository/StudyUserRepository.java
@@ -7,10 +7,15 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import navik.domain.study.entity.StudyUser;
+import navik.domain.users.entity.User;
 
 public interface StudyUserRepository extends JpaRepository<StudyUser, Long> {
 	// 여러 스터디의 현재 참여 인원수를 한번에 조회한다
 	@Query("SELECT su.study.id, COUNT(su) " + "FROM StudyUser su " +
 		"WHERE su.study.id IN :studyIds " + "AND su.attend = 'ACCEPTANCE' " + "GROUP BY su.study.id")
 	List<Object[]> countParticipantsByStudyIds(@Param("studyIds") List<Long> studyIds);
+
+	// 해당 스터디에 참여하는 인원 조회
+	@Query("SELECT sm.user FROM StudyUser sm WHERE sm.study.id = :studyId")
+	List<User> findUserByStudyId(@Param("studyId") Long studyId);
 }
