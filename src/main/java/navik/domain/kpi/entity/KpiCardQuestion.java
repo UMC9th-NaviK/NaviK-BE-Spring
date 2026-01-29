0d16b63d6f548bf0b74b747d9124df1a61c18679
pywoo <kfdsy0103@naver.com>
2026-01-29 20:36:03 +0900

    feat: kpi card별 질문 엔티티 추가

---
+++ src/main/java/navik/domain/kpi/entity/KpiCardQuestion.java
@@ -0,0 +1,36 @@
+package navik.domain.kpi.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import navik.global.entity.BaseEntity;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+@Table(name = "kpi_card_questions")
+public class KpiCardQuestion extends BaseEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "content", nullable = false)
+	private String content;
+
+	@OneToOne
+	@JoinColumn(name = "kpi_card_id")
+	private KpiCard kpiCard;
+}
