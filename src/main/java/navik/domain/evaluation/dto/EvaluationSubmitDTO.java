b36e632f9203259ad3e1e34fcf65b1e7fe9c95cf
LeeJaeJun1 <jj030207@naver.com>
2026-02-01 01:09:34 +0900

    feat: 스퍼티 평가하기 DTO 생성

--- src/main/java/navik/domain/evaluation/dto/EvaluationSubmitDTO.java
+++ src/main/java/navik/domain/evaluation/dto/EvaluationSubmitDTO.java
@@ -2,29 +2,27 @@ package navik.domain.evaluation.dto;
 
 import java.util.List;
 
-import jakarta.validation.constraints.Max;
-import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
 
-public class EvaluationSubmitDTO {
+public record EvaluationSubmitDTO(
+	@NotNull
+	Long targetUserId,
 
-	@Getter
-	@NoArgsConstructor
-	public static class EvaluationSubmit {
-		@NotNull
-		private Long targetUserId;
-		@NotNull
-		@Min(1)
-		@Max(5)
-		private Float score;
-		@Size(min = 5, max = 5, message = "이런 점이 뛰어나요 태그 5개를 선택해야 합니다.")
-		private List<Long> strengthTagIds;
-		@Size(min = 5, max = 5, message = "보완하면 좋아요 태그 5개를 선택해야 합니다.")
-		private List<Long> weaknessTagIds;
+	@NotNull
+	@DecimalMin(value = "1.0")
+	@DecimalMax(value = "5.0")
+	Float score,
 
-		private String advice;
-	}
+	@Size(min = 5, max = 5, message = "이런 점이 뛰어나요 태그 5개를 선택해야 합니다.")
+	List<Long> strengthTagIds,
+
+	@Size(min = 5, max = 5, message = "보완하면 좋아요 태그 5개를 선택해야 합니다.")
+	List<Long> weaknessTagIds,
+
+	String advice
+) {
 }
+
