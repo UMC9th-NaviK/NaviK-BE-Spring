51299a8771b23fd2241a8c4db5c7439b7df79f3b
pywoo <kfdsy0103@naver.com>
2026-01-31 21:21:46 +0900

    feat: endDate 기반 study 조회 쿼리 작성

--- src/main/java/navik/domain/study/repository/StudyRepository.java
+++ src/main/java/navik/domain/study/repository/StudyRepository.java
@@ -1,8 +1,21 @@
 package navik.domain.study.repository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import navik.domain.study.entity.Study;
 
 public interface StudyRepository extends JpaRepository<Study, Long> {
+
+	@Query("""
+		    SELECT s FROM Study s
+		    JOIN FETCH s.studyUsers su
+		    JOIN FETCH su.user
+		    WHERE s.endDate = :endDate
+		""")
+	List<Study> findAllByEndDateWithStudyUser(@Param("endDate") LocalDate endDate);
 }
