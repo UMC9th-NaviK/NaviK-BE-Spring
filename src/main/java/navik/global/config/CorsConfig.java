fbddbebaf1d79a51e337b6a3e0b88ed5186cd2c2
junho <2171168@hansung.ac.kr>
2026-02-01 17:41:30 +0900

    fix: cors 설정

--- src/main/java/navik/global/config/CorsConfig.java
+++ src/main/java/navik/global/config/CorsConfig.java
@@ -13,14 +13,11 @@ public class CorsConfig {
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration config = new CorsConfiguration();
-		config.setAllowedOrigins(List.of(
-			"http://localhost:3000",
-			"http://localhost:8080",
-			"https://api.navik.kro.kr"
-		));  // 프론트 실배포 추가
+		config.setAllowedOrigins(
+			List.of("http://localhost:5173", "http://localhost:8080", "https://api.navik.kro.kr"));  // 프론트 실배포 추가
 		config.setAllowCredentials(true);  // 쿠키 전송 허용
 		config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
-		config.setAllowedHeaders(List.of("*"));
+		config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
 
 		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		source.registerCorsConfiguration("/**", config);
