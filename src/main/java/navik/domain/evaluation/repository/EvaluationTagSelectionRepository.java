49824c9009ea9ce83fe70d2305b1ff70a0d59d63
LeeJaeJun1 <jj030207@naver.com>
2026-02-02 16:48:26 +0900

    feat: 평가 때 선택한 태그 관련 내용들 모두 조회하는 레포지터리 생성

--- src/main/java/navik/domain/evaluation/repository/EvaluationTagSelectionRepository.java
+++ src/main/java/navik/domain/evaluation/repository/EvaluationTagSelectionRepository.java
@@ -1,8 +1,23 @@
 package navik.domain.evaluation.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import navik.domain.evaluation.entity.Evaluation;
 import navik.domain.evaluation.entity.EvaluationTagSelection;
 
 public interface EvaluationTagSelectionRepository extends JpaRepository<EvaluationTagSelection, Long> {
+	// 여러 평가들에 속한 모든 태그 매핑 정보와 태그 엔티티를 한 번에 조회 (Fetch Join)
+	@Query(
+		"""
+			SELECT ets 
+			FROM EvaluationTagSelection ets 
+			JOIN FETCH ets.tag
+			WHERE ets.evaluation 
+			IN :evaluations
+			""")
+	List<EvaluationTagSelection> findAllByEvaluationIn(@Param("evaluations") List<Evaluation> evaluations);
 }
