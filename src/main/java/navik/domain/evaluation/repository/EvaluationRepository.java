87f4c83cd35227ef78e4d2dbeff355f91725fef1
LeeJaeJun1 <jj030207@naver.com>
2026-02-02 16:47:40 +0900

    feat: 내가 받은 평가 모두 조회하는 레포지토리 생성

--- src/main/java/navik/domain/evaluation/repository/EvaluationRepository.java
+++ src/main/java/navik/domain/evaluation/repository/EvaluationRepository.java
@@ -1,9 +1,23 @@
 package navik.domain.evaluation.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import navik.domain.evaluation.entity.Evaluation;
 
 public interface EvaluationRepository extends JpaRepository<Evaluation, Long> {
 	boolean existsByStudyIdAndEvaluatorIdAndEvaluateeId(Long studyId, Long evaluatorId, Long evaluateeId);
+
+	// evaluatee.id를 기준으로 내가 받은 평가 모두 조회
+	// submitEvaluation에서 평가 내용을 evaluatee에 저장했기 때문에
+	@Query(
+		"""
+			SELECT e 
+			FROM Evaluation e 
+			WHERE e.evaluatee.id = :userId
+			"""
+	)
+	List<Evaluation> findAllByEvaluateeId(Long userId);
 }
