5615f4d293327453cb6865b15e2a1d69b5d64491
junho <2171168@hansung.ac.kr>
2026-02-05 04:20:13 +0900

    feat: 예외처리

--- src/main/java/navik/domain/growthLog/notion/exception/code/NotionErrorCode.java
+++ src/main/java/navik/domain/growthLog/notion/exception/code/NotionErrorCode.java
@@ -1,10 +1,10 @@
 package navik.domain.growthLog.notion.exception.code;
 
+import org.springframework.http.HttpStatus;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import navik.global.apiPayload.exception.code.BaseCode;
-
-import org.springframework.http.HttpStatus;
 
 @Getter
 @AllArgsConstructor
@@ -13,22 +13,23 @@ public enum NotionErrorCode implements BaseCode {
 	/**
 	 * 사용자가 Notion 연동 권한 부여를 거부한 경우
 	 */
-	OAUTH_DENIED(HttpStatus.BAD_REQUEST, "NOTION4001", "Notion 연동이 거부되었습니다."),
+	OAUTH_DENIED(HttpStatus.BAD_REQUEST, "NOTION400_1", "Notion 연동이 거부되었습니다."),
 
 	/**
 	 * OAuth 콜백에서 state 파라미터가 누락된 경우
 	 */
-	OAUTH_STATE_MISSING(HttpStatus.BAD_REQUEST, "NOTION4002", "잘못된 요청입니다. (state 누락)"),
+	OAUTH_STATE_MISSING(HttpStatus.BAD_REQUEST, "NOTION400_2", "잘못된 요청입니다. (state 누락)"),
 
 	/**
 	 * state에서 userId 파싱 실패 (숫자 형식 아님)
 	 */
-	OAUTH_INVALID_USER_ID(HttpStatus.BAD_REQUEST, "NOTION4003", "잘못된 userId 형식입니다."),
+	OAUTH_INVALID_USER_ID(HttpStatus.BAD_REQUEST, "NOTION400_3", "잘못된 userId 형식입니다."),
 
 	/**
 	 * Notion OAuth 토큰 교환 실패
 	 */
-	OAUTH_TOKEN_EXCHANGE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "NOTION5001", "Notion 연동 중 오류가 발생했습니다.");
+	OAUTH_TOKEN_EXCHANGE_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "NOTION500_1", "Notion 연동 중 오류가 발생했습니다."),
+	NOTION_OAUTH_RESPONSE_NULL(HttpStatus.INTERNAL_SERVER_ERROR, "NOTION_500_2", "Notion OAuth 토큰 교환 응답이 null입니다.");
 
 	private final HttpStatus httpStatus;
 	private final String code;
