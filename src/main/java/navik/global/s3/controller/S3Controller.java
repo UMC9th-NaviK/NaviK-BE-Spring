e30351b58d18575eedec7e1bedb09a630195863b
pywoo <kfdsy0103@naver.com>
2026-02-07 22:02:09 +0900

    fix: S3Dto > S3DTO

--- src/main/java/navik/global/s3/controller/S3Controller.java
+++ src/main/java/navik/global/s3/controller/S3Controller.java
@@ -1,20 +1,20 @@
 package navik.global.s3.controller;
 
-import navik.global.apiPayload.ApiResponse;
-import navik.global.apiPayload.code.status.GeneralSuccessCode;
-import navik.global.s3.S3PathType;
-import navik.global.s3.dto.S3Dto;
-import navik.global.s3.service.S3Service;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.tags.Tag;
-import lombok.RequiredArgsConstructor;
-
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import navik.global.apiPayload.ApiResponse;
+import navik.global.apiPayload.code.status.GeneralSuccessCode;
+import navik.global.s3.S3PathType;
+import navik.global.s3.dto.S3DTO;
+import navik.global.s3.service.S3Service;
 
 @RestController
 @RequiredArgsConstructor
@@ -27,7 +27,7 @@ public class S3Controller {
 
 	@GetMapping("/presigned-url")
 	@Operation(summary = "Presigned URL 생성", description = "S3 업로드를 위한 Presigned URL을 생성합니다.(key = ImageUrl)")
-	public ApiResponse<S3Dto.PreSignedUrlResponse> getPresignedUrl(
+	public ApiResponse<S3DTO.PreSignedUrlResponse> getPresignedUrl(
 		@Parameter(description = "S3 경로 타입", example = "PORTFOLIO_PDF") @RequestParam S3PathType pathType,
 		@Parameter(description = "경로에 사용될 ID (userId, boardId 등)", example = "1") @RequestParam Long id,
 		@Parameter(description = "파일 확장자", example = ".pdf") @RequestParam String extension) {
