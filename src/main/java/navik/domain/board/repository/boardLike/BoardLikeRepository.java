1da0921991233a36308295f96ee23a5b359b1463
LeeJaeJun1 <jj030207@naver.com>
2026-02-08 18:33:59 +0900

    fix: countLikeByBoard 레포지터리 삭제

--- src/main/java/navik/domain/board/repository/boardLike/BoardLikeRepository.java
+++ src/main/java/navik/domain/board/repository/boardLike/BoardLikeRepository.java
@@ -16,8 +16,6 @@ import navik.domain.users.entity.User;
 public interface BoardLikeRepository extends JpaRepository<BoardLike, Long> {
 	Optional<BoardLike> findByBoardAndUser(Board board, User user); // 삭제를 위해 엔티티를 직접 조회하는 메서드
 
-	Integer countLikeByBoard(Board board); // 게시글 좋아요 총합
-
 	@Query("select bl.board.id, COUNT(bl) FROM BoardLike bl where bl.board.id IN :boardIds GROUP BY bl.board.id")
 	List<Object[]> countByBoardIdIn(@Param("boardIds") List<Long> boardIds);
 }
