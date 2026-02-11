1fbc05fa76f8375420b5cf39b6e67608fdfbe756
pywoo <kfdsy0103@naver.com>
2026-02-12 00:57:34 +0900

    fix: goal

---
+++ src/main/java/navik/global/config/SchedulerConfig.java
@@ -0,0 +1,24 @@
+package navik.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.SchedulingConfigurer;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskScheduler;
+import org.springframework.scheduling.config.ScheduledTaskRegistrar;
+
+@Configuration
+@EnableScheduling
+public class SchedulerConfig implements SchedulingConfigurer {
+
+	@Override
+	public void configureTasks(ScheduledTaskRegistrar taskRegistrar) {
+		ThreadPoolTaskScheduler threadPoolTaskScheduler = new ThreadPoolTaskScheduler();
+		
+		int poolSize = 5;
+		threadPoolTaskScheduler.setPoolSize(poolSize);
+		threadPoolTaskScheduler.setThreadNamePrefix("navik-scheduler-");
+		threadPoolTaskScheduler.initialize();
+
+		taskRegistrar.setTaskScheduler(threadPoolTaskScheduler);
+	}
+}
