6523a420683a738967728f2967420a80bf6914ba
bmh7190 <bmh7190@naver.com>
2026-02-11 18:01:48 +0900

    refactor: Level 엔티티를 통해 반환하도록 기존 로직 수정

--- src/main/java/navik/domain/kpi/event/policy/LevelPolicy.java
+++ src/main/java/navik/domain/kpi/event/policy/LevelPolicy.java
@@ -7,20 +7,13 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Component
 public class LevelPolicy {
+
+	// 총 점수로 레벨 반환
 	public int calculateLevel(Long totalScore) {
-		if(totalScore == null){
+		if (totalScore == null) {
 			log.warn("totalScore is null.");
 			return 0;
 		}
-		if (totalScore >= 4801) return 10;
-		if (totalScore >= 3701) return 9;
-		if (totalScore >= 2801) return 8;
-		if (totalScore >= 2101) return 7;
-		if (totalScore >= 1551) return 6;
-		if (totalScore >= 1101) return 5;
-		if (totalScore >= 751)  return 4;
-		if (totalScore >= 451)  return 3;
-		if (totalScore >= 201)  return 2;
-		return 1;
+		return Level.fromScore(totalScore).getValue();
 	}
 }
